14e0d7a790532e7b38eeb78b6946c93398ea1009
misztijanos <misztijanos@yahoo.com>
2021-03-06 13:25:37 +0200

    added basic jsx + css

--- src/App.js
+++ src/App.js
@@ -4,6 +4,20 @@ function App() {
   return (
     <div className="container">
       <h1 className="headline">Time the Robotalk</h1>
+      <div className="phrases">
+        <div className="phrase">
+          <input className="num-input" type="number" />
+          <input className="text-input" type="text" />
+        </div>
+        <div className="phrase">
+          <input className="num-input" type="number" />
+          <input className="text-input" type="text" />
+        </div>
+      </div>
+      <button>Add</button>
+      <h1 className="timer">0</h1>
+      <button className="start">Start</button>
+      <button className="stop">Stop</button>
     </div>
   );
 }
