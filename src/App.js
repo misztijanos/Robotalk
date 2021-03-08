5197ee3a03abac99e8d066cd64cdf811f2cc2f36
misztijanos <misztijanos@yahoo.com>
2021-03-08 09:15:14 +0200

    finished

--- src/App.js
+++ src/App.js
@@ -1,24 +1,50 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import "./App.css";
 import Phrase from "./Phrase";
 import { useStopwatch } from "react-timer-hook";
+import { useSpeechSynthesis } from "react-speech-kit";
 
 function App() {
   const [phrases, setPhrases] = useState([
-    { time: 1, text: "Hi!" },
-    { time: 2, text: "Type your text here." },
+    { time: 1, text: "Hi!", spoken: false },
+    { time: 2, text: "Type your text here.", spoken: false },
   ]);
   const { seconds, isRunning, start, reset } = useStopwatch({
     autoStart: false,
   });
+  const { speak, supported } = useSpeechSynthesis();
 
+  useEffect(() => {
+    if (isRunning) {
+      const currentIndex = phrases.findIndex(
+        (phrase) => phrase.time === seconds
+      );
+      if (currentIndex !== -1 && !phrases[currentIndex].spoken) {
+        phrases[currentIndex].spoken = true;
+        speak(phrases[currentIndex]);
+      }
+    }
+    const maxTime = phrases.reduce(
+      (max, curr) => (curr.time > max ? curr.time : max),
+      0
+    );
+    if (seconds > maxTime) {
+      reset();
+      resetSpoken();
+    }
+    //check when it's time to shut down
+  }, [isRunning, seconds, phrases]);
+
+  function resetSpoken() {
+    const newPhrases = phrases.map((curr) => ({ ...curr, spoken: false }));
+    setPhrases(newPhrases);
+  }
   function handleChange(index, e) {
     const newPhrases = [...phrases];
     //the number needs some validation
     e.target.type === "number" &&
       (newPhrases[index].time = Number(e.target.value));
     newPhrases[index].time < 0 && (newPhrases[index].time = 0);
-
     e.target.type === "text" && (newPhrases[index].text = e.target.value);
 
     setPhrases(newPhrases);
@@ -30,7 +56,11 @@ function App() {
         ? [{ time: 1, text: "Hi!" }]
         : [
             ...phrases,
-            { time: phrases[phrases.length - 1].time + 1, text: "" },
+            {
+              time: phrases[phrases.length - 1].time + 1,
+              text: "",
+              spoken: false,
+            },
           ];
     setPhrases(newPhrases);
   }
@@ -39,6 +69,10 @@ function App() {
     const newPhrases = [...phrases];
     newPhrases.splice(index, 1);
     setPhrases(newPhrases);
+  }
+
+  if (!supported) {
+    return "Your browser doesn't support the SpeechSynthesis API";
   }
 
   return (
@@ -62,7 +96,13 @@ function App() {
           Start
         </button>
       ) : (
-        <button className="stop" onClick={reset}>
+        <button
+          className="stop"
+          onClick={() => {
+            reset();
+            resetSpoken();
+          }}
+        >
           Stop
         </button>
       )}
